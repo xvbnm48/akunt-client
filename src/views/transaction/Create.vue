c355cf1bc464edeb85cf2c6005dce1779d246d3d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-06-08 23:21:16 +0700

    membuat create data, dan validation error

--- src/views/transaction/Create.vue
+++ src/views/transaction/Create.vue
@@ -1,7 +1,106 @@
 <template>
-  <h3>Create</h3>
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <router-link
+          :to="{ name: 'transaction.index' }"
+          class="btn btn-primary btn-sm rounded mb-3"
+        >
+          BACK</router-link
+        >
+        <div class="card rounded shadow">
+          <div class="card-header">Edit Transaction</div>
+          <div class="card-body">
+            <form @submit.prevent="store()">
+              <div class="mb-3">
+                <label for="" class="form-label"> Title </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="transaction.title"
+                />
+                <div class="text-danger" v-if="validation.title">
+                  {{ validation.title[0] }}
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label"> Amount </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  v-model="transaction.amount"
+                />
+                <div v-if="validation.amount" class="text-danger">
+                  {{ validation.amount[0] }}
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label"> Time </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="yyyy-mm-dd hh:mm:ss"
+                  v-model="transaction.time"
+                />
+                <div class="text-danger" v-if="validation.time">
+                  {{ validation.time[0] }}
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label"> Type </label>
+                <select class="form-select" id="" v-model="transaction.type">
+                  <option value="expense">Expense</option>
+                  <option value="revenue">Revenue</option>
+                </select>
+                <div class="text-danger" v-if="validation.type">
+                  {{ validation.type[0] }}
+                </div>
+              </div>
+              <button class="btn btn-outline-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+export default {
+  setup() {
+    //data binding
+    const transaction = reactive({
+      title: "",
+      amount: "",
+      time: "",
+      type: "",
+    });
+
+    const validation = ref([]);
+
+    const router = useRouter();
+    function store() {
+      axios
+        .post("http://127.0.0.1:8000/api/transaction", transaction)
+        .then(() => {
+          router.push({
+            name: "transaction.index",
+          });
+        })
+        .catch((err) => {
+          validation.value = err.response.data;
+        });
+    }
+
+    return {
+      transaction,
+      validation,
+      router,
+      store,
+    };
+  },
+};
 </script>
