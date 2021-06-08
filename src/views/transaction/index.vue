db6422b1a76f818b812f0d34e856a3ca6d8c6ce4
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-06-08 21:11:33 +0700

    fix error pada vue,ga bisa baca viewrouter,fixnya dega nreistall npm dan reinstall router-view

---
+++ src/views/transaction/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <router-link
+          :to="{ name: 'transaction.create' }"
+          class="btn btn-primary btn-sm rounded mb-3"
+        >
+          ADD</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
